ef52da3798754d7f23f26fc07ce8da7d022aabaa
ovogmap <djtheend@naver.com>
2021-03-23 23:34:45 +0900

    테스트 케이스 작성

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const port = 3000;
-const users = [
+var express = require("express");
+var app = express();
+var morgan = require("morgan");
+var port = 3000;
+let users = [
   { id: 1, name: "alice" },
   { id: 2, name: "bek" },
   { id: 3, name: "chris" },
 ];
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/users", (req, res) => {
   req.query.limit = req.query.limit || users.length;
@@ -21,7 +23,9 @@ app.get("/users", (req, res) => {
 
 app.get("/users/:id", (req, res) => {
   const id = parseInt(req.params.id, 10);
-  if (Number.isNaN(id)) return res.status(400).end();
+  if (Number.isNaN(id)) {
+    return res.status(400).end();
+  }
 
   const user = users.filter((user) => user.id === id)[0];
   if (!user) return res.status(404).end();
@@ -31,9 +35,45 @@ app.get("/users/:id", (req, res) => {
 
 app.delete("/users/:id", (req, res) => {
   const id = parseInt(req.params.id, 10);
-  // if (Number.isNaN(id)) return res.status(400).end();
+  if (Number.isNaN(id)) {
+    return res.status(400).end();
+  }
   users = users.filter((user) => user.id !== id);
   res.status(204).end();
+});
+
+app.post("/users", (req, res) => {
+  // console.log(req.body.name);
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+
+  const isConflict = users.filter((user) => user.name === name).length;
+  if (isConflict) return res.status(409).end();
+
+  const id = Date.now();
+  const user = { id, name };
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+
+  const isConflict = users.filter((user) => user.name === name).length;
+  if (isConflict) return res.status(409).end();
+
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
+
+  user.name = name;
+
+  // const user = { id, name };
+  // users = users.map((v) => (v.id === user.id ? user : v));
+  res.status(201).json(user);
 });
 
 app.listen(port, () => {
--- app.spce.js
+++ app.spce.js
@@ -31,7 +31,7 @@ describe("GET /users는", () => {
   });
 });
 
-describe("GET /users/1 은", () => {
+describe("GET /users/:id 은", () => {
   describe("성공시", () => {
     it("id가 1인 유저객체를 반환한다", (done) => {
       request(app)
@@ -45,24 +45,88 @@ describe("GET /users/1 은", () => {
 
   describe("실패시", () => {
     it("id가 숫자가 아니면 400을 응답한다", (done) => {
-      request(app).delete("/users/one").expect(400).end(done);
+      request(app).get("/users/one").expect(400).end(done);
     });
     it("id로 유저를 찾을수 없을 경우 404로 응답한다", (done) => {
-      request(app).delete("/users/4").expect(404).end(done);
+      request(app).get("/users/4").expect(404).end(done);
     });
   });
 });
 
-describe("GET /users/1은", () => {
+describe("DELETE /users/:id 은", () => {
   describe("성공시", () => {
     it("204를 응답한다.", (done) => {
       request(app).delete("/users/1").expect(204).end(done);
     });
   });
 
-  // describe("실패시", () => {
-  //   it("id 가 숫자가 아닐경우 400으로 응답한다", (doen) => {
-  //     request(app).delete("/users/one").expect(400).end(done);
-  //   });
-  // });
+  describe("실패시", () => {
+    it("id 가 숫자가 아닐경우 400으로 응답한다", (done) => {
+      request(app).delete("/users/one").expect(400).end(done);
+    });
+  });
+});
+
+describe("POST /users 는", () => {
+  describe("성공시", () => {
+    let name = "hyo",
+      body;
+    before((done) => {
+      request(app)
+        .post("/users")
+        .send({ name })
+        .expect(201)
+        .end((err, res) => {
+          body = res.body;
+          done();
+        });
+    });
+
+    it("생성된 유저 객체를 반환한다", () => {
+      body.should.have.property("id");
+    });
+    it("입력한 name값을 반환한다", () => {
+      body.should.have.property("name", name);
+    });
+  });
+
+  describe("실패시", () => {
+    it("name 파라미터 누락시 400을 반환한다", (done) => {
+      request(app).post("/users").send({}).expect(400).end(done);
+    });
+
+    it("name이 중복인 경우 409를 반환한다", (done) => {
+      request(app).post("/users").send({ name: "hyo" }).expect(409).end(done);
+    });
+  });
+});
+
+describe("PUT /user/:id 는", () => {
+  describe("성공시", () => {
+    it("변견된 name을 반환한다.", (done) => {
+      const name = "jung";
+      request(app)
+        .put("/users/3")
+        .send({ name })
+        .end((err, res) => {
+          res.body.should.have.property("name", name);
+          done();
+        });
+    });
+  });
+
+  describe("실패시", () => {
+    it("정수가 아닌 id일 경우 400을 응답한다", (done) => {
+      request(app).put("/users/one").expect(400).end(done);
+    });
+    it("name이 없을 경우 400을 응답", (done) => {
+      request(app).put("/users/3").send().expect(400).end(done);
+    });
+    it("없는 유저일 경우 404를 응답", (done) => {
+      request(app).put("/users/77").send({ name: "foo" }).expect(404).end(done);
+    });
+    it("이름이 중복일 경우 409를 응답", (done) => {
+      request(app).put("/users/3").send({ name: "bek" }).expect(409).end(done);
+    });
+  });
 });
